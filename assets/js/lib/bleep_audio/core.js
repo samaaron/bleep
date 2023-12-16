d48d7027d087257ac87ac484f3027fe92df87218
guyjbrown <g.j.brown@sheffield.ac.uk>
2023-12-16 14:28:37 +0000

    removed effects wrapper and associated comments

--- assets/js/lib/bleep_audio/core.js
+++ assets/js/lib/bleep_audio/core.js
@@ -162,37 +162,6 @@ export default class BleepAudioCore {
 
     const pitchHz = Utility.midiNoteToHz(note);
 
-    // demo of how to create effects
-    // const fx = new EffectsChain(this.#audio_context, this.#monitor);
-
-    //fx.add(this.#reverb, 0.05, 0.8);
-
-    //this.#chorus.rate = 2.2;
-    //this.#chorus.depth = 2;
-    //this.#chorus.spread = 0.95;
-
-    //this.#delay.leftDelay = 0.25;
-    //this.#delay.rightDelay = 0.75;
-    //this.#delay.feedback = 0.2;
-
-    // the second parameter is the mix level (0 = fully dry, 1 = fully wet)
-    // the third parameter controls the output level of this effect (and hence the remainder of the chain)
-    // which is useful to stop amplitudes getting too big (which leads to crackles and clicks)
-
-    //fx.add(this.#delay, 0.3, 0.8);
-    //fx.add(this.#chorus, 0.2, 0.8);
-    //fx.add(this.#reverb, 0.2, 0.8);
-
-    // here we've made global effects, and I think we'll want to do this for reverb since
-    // convolution is expensive
-    // the others (delay, chorus) are cheaper to run and could be created for each note if we dont
-    // want them shared, e.g.
-    // fx.add(new RolandChorus(),0.2,0.8) etc etc
-
-    // create a player, passing in the fx chain
-    // we pass the fx chain so that it can be disposed of when we finish playing
-    // if fx is undefined then the output of the synth is patched directly to player.out
-
     const gen = this.#getSynthGen(synthdef_id);
     let synth = new Player(
       this.#audio_context,
@@ -212,8 +181,6 @@ export default class BleepAudioCore {
     // play the note
     synth.play(audio_context_sched_s);
 
-    // after the note has played the effects chain (but not any global effects it contains)
-    // is disposed, after a delay that lets delays run out and reverb tails finish
   }
 
   loadSynthDef(synthdef) {
