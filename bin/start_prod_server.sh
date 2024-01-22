640a74e46ee2cb6c4cc20521be93039ac0564c74
Sam Aaron <samaaron@gmail.com>
2024-01-22 13:12:31 +0000

    Boot - improve server boot script

--- bin/start_prod_server.sh
+++ bin/start_prod_server.sh
@@ -2,11 +2,20 @@
 set -e # Quit script on error
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKING_DIR="$(pwd)"
-cd "${SCRIPT_DIR}"../
+cd "${SCRIPT_DIR}"/../
 
-## Uncomment and insert the correct values for cert-path, key-path and host name.
+## Insert the correct values for cert-path, key-path and host name.
+PHX_SERVER=true
+BLEEP_SECURE_SERVER=true
+HTTPS_PORT=443
+BLEEP_SSL_CERT_PATH='cert-path'
+BLEEP_SSL_KEY_PATH='key-path'
+PHX_HOST=example.com
+MIX_ENV=prod
+SECRET_KEY_BASE=`mix phx.gen.secret`
 
-# sudo PHX_SERVER=true BLEEP_SECURE_SERVER=true BLEEP_SSL_CERT_PATH='cert-path' BLEEP_SSL_KEY_PATH='key-path' SECRET_KEY_BASE=`mix phx.gen.secret` PHX_HOST=example.com HTTPS_PORT=443 MIX_ENV=prod elixir -S mix phx.server
+mix assets.deploy
+elixir -S mix phx.server
 
 # Restore working directory as it was prior to this script running...
 cd "${WORKING_DIR}"
