55447359ba3dad8fe78880567d00bcf99737aff9
Sam Aaron <samaaron@gmail.com>
2024-04-08 22:23:15 +0100

    Time Sync - remove deprecated channel comms

--- lib/bleep_web/channels/bleep_time_sync_channel.ex
+++ lib/bleep_web/channels/bleep_time_sync_channel.ex
@@ -24,25 +24,4 @@ defmodule BleepWeb.BleepTimeSyncChannel do
 
     {:reply, {:ok, msg}, socket}
   end
-
-  @impl true
-  def handle_in("time-sync", %{"time_s" => time_s, "latency_s" => latency_s}, socket) do
-    socket = assign(socket, :kalman, Kalman.step(0, latency_s, socket.assigns.kalman))
-    latency_est_s = Kalman.estimate(socket.assigns.kalman)
-
-    latency_est_ms = latency_est_s * 1000
-
-    msg = %{
-      client_timestamp: time_s,
-      latency_est: latency_est_s,
-      server_timestamp: :erlang.system_time(:milli_seconds) / 1000.0
-    }
-
-    {:reply, {:ok, msg}, socket}
-  end
-
-  # Add authorization logic here as required.
-  # defp authorized?(_payload) do
-  #   true
-  # end
 end
