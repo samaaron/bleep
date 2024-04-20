df86dcf8f73a32b6f16624b0c902fc7a967c3677
Guy Brown <g.j.brown@sheffield.ac.uk>
2024-04-20 09:48:34 +0100

    kes page updated for Monday

--- priv/content/kes.lua
+++ priv/content/kes.lua
@@ -7,7 +7,7 @@ markdown [[
 ]],
 
 markdown [[
-Exercise 1 : First Bleep
+## Exercise 1 : Distortion
 ]],
 
 editor [[
@@ -15,7 +15,7 @@ editor [[
 ]],
 
 markdown [[
-Exercise 2 : Sleep
+## Exercise 2 : Reverb
 ]],
 
 editor [[
@@ -23,7 +23,7 @@ editor [[
 ]],
 
 markdown [[
-Exercise 3 : Samples
+## Exercise 3 : Your own song
 ]],
 
 editor [[
@@ -31,40 +31,68 @@ editor [[
 ]],
 
 markdown [[
-Exercise 4 : Iteration
-]],
-
-editor [[
-
+## Survey
+https://forms.gle/MHDUx8j7hRL2tCGb6
 ]],
 
 markdown [[
-Exercise 5 : Iteration with arrays
-]],
-
-editor [[
-
-]],
-
-markdown [[
-Exercise 6 : Fix the code!
-]],
-
-editor [[
-notes = {C3, D3, E3, F3
-while i = 1,4,3
-do
-Play(notes[i])
-        sleep(1)
-]],
-
-markdown [[
-Challenge exercise
-]],
-
-editor [[
-
-]],
-
+## List of sound samples
+* bass_hit_c
+* drum_bass_soft
+* elec_bleep
+* loop_safari
+* loop_breakbeat
+* loop_amen
+* loop_electric
+* loop_garzul
+* loop_industrial
+* bass_dnb_f
+* bass_drop_c
+* bass_hard_c
+* bass_hit_c
+* bass_thick_c
+* bass_trance_c
+* drum_bass_soft
+* drum_cowbell
+* drum_cymbal_closed
+* drum_cymbal_hard
+* drum_cymbal_open
+* drum_cymbal_pedal
+* drum_cymbal_soft
+* drum_heavy_kick
+* drum_roll
+* elec_beep
+* elec_bell
+* elec_blip
+* elec_blip2
+* elec_blup
+* elec_bong
+* elec_chime
+* elec_cymbal
+* elec_filt_snare
+* elec_flip
+* elec_fuzz_tom
+* elec_hi_snare
+* elec_hollow_kik
+* loop_industrial
+* loop_mehackit1
+* loop_mehackit2
+* loop_mika
+* loop_perc1
+* loop_perc2
+* loop_safari
+* loop_tabla
+* loop_weirdo
+* mehackit_robot1
+* mehackit_robot2
+* perc_bell
+* perc_bell2
+* perc_door
+* perc_impact1
+* vinyl_backspin
+* vinyl_hiss
+* vinyl_rewind
+* vinyl_scratch
+]]
 
 }
