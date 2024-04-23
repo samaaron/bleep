c9b75c169826a3122f74abe528850202e96b5022
Sam Aaron <samaaron@gmail.com>
2024-04-23 10:18:33 +0100

    Content - add both KES lessons side by side
    (they're both currently being taught simultaneously so both need to be available)

---
+++ priv/content/kes1.lua
@@ -0,0 +1,71 @@
+bpm = 120
+
+content = {
+
+markdown [[
+# Bleep : King Ecgbert School
+## Lesson 1
+]],
+
+markdown [[
+Exercise 1 : First Bleep
+]],
+
+editor [[
+
+]],
+
+markdown [[
+Exercise 2 : Sleep
+]],
+
+editor [[
+
+]],
+
+markdown [[
+Exercise 3 : Samples
+]],
+
+editor [[
+
+]],
+
+markdown [[
+Exercise 4 : Iteration
+]],
+
+editor [[
+
+]],
+
+markdown [[
+Exercise 5 : Iteration with arrays
+]],
+
+editor [[
+
+]],
+
+markdown [[
+Exercise 6 : Fix the code!
+]],
+
+editor [[
+notes = {C3, D3, E3, F3
+while i = 1,4,3
+do
+Play(notes[i])
+        sleep(1)
+]],
+
+markdown [[
+Challenge exercise
+]],
+
+editor [[
+
+]],
+
+
+}
