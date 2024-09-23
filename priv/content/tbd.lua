8ef4bff1eb1db7939b6a2ab85e5afeef41b29cba
guyjbrown <g.j.brown@sheffield.ac.uk>
2024-09-23 12:11:11 +0100

    added link to workbook and interlinked the tutorial and seclusion sites

--- priv/content/tbd.lua
+++ priv/content/tbd.lua
@@ -12,6 +12,10 @@ Welcome to this online tutorial on techno music programming, brought to you by *
 ]],
 
 markdown [[
+An accompanying workbook for these tutorials is availabe: [click here for the workbook](https://drive.google.com/file/d/1glIlPp00SJi_gMVX8bZ5fwLNumnrlVyy/view?usp=drive_link).
+]],
+
+markdown [[
 * [Playing samples](/artist/tbd1)
 * [Playing synths](/artist/tbd2)
 * [Using effects](/artist/tbd3)
@@ -20,6 +24,12 @@ markdown [[
 * [Making a bass line](/artist/tbd6)
 * [Atmosphere and structure](/artist/tbd7)
 * [Mixing and balancing parts](/artist/tbd8)
+]],
+
+markdown [[
+### Find out more
+Code from the performance by The Black Dog at the Festival of the Mind on 21st September 2024 is available, 
+together with notes from the band and links for further information. [Click here to find out more](/artist/seclusion).
 ]],
 
 markdown [[
